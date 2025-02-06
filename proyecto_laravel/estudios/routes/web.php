0dfda58423d4fdb1b0462a15632d82f76c722696
FelipeOrtiz <ortiz15243u@gmail.com>
2025-02-06 11:59:26 +0100

    Subida primer proyecto laravel

---
+++ proyecto_laravel/estudios/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\Route;
+
+/*
+Route::get("/main", function () {
+    return "<h1>Hola con h1</h1>";
+    //return view("main");
+});
+*/
+
+//usando una función flecha
+/*
+Route::get("/main", fn()=>"<h1>Hola</h1>");
+Route::get('/main', fn() => view('main'));
+*/
+
+//Route::view('/main', 'index');
+
+Route::get ("/main", [MainController::class, "index"]);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/alumnos/{numero}', function ($num) {
+    return view('alumnos', ['numero' => $num]);
+})->where('numero', '[0-9]+');
+
+Route::fallback(function () {
+   $msj = request()->getRequestUri();
+   return view("<h1>Error 404: <span style='color: red'>$msj</span></h1>");
+});
